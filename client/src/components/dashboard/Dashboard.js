26231d70d00d9b5296f9c7b3d64c6a2d88f69d3f
tyler stuart <t.stu21.11@gmail.com>
2019-07-29 00:40:33 -0700

    test

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,88 @@
+// Here we fetch all of our data using an action,
+// bring it in from the Redux state, and
+// pass it down to other components
+
+import React, { Fragment, useEffect } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Spinner from "../layout/Spinner";
+import DashboardActions from "./DashboardActions";
+import DashboardAdminActions from "./DashboardAdminActions";
+import Experience from "./Experience";
+import Education from "./Education";
+// import Company from "./Company";
+import { getCurrentProfile, deleteAccount } from "../../actions/profile";
+// import ComAccounts from './ComAccounts';
+
+// const Dashboard = props => {
+const Dashboard = ({
+  getCurrentProfile,
+  deleteAccount,
+  auth: { user },
+  profile: { profile, loading }
+}) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, [getCurrentProfile]);
+
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Welcome {user && user.name}
+      </p>
+      {profile !== null ? (
+        <Fragment>
+          <DashboardActions />
+          {/* experience is the prop we pass into Experience.js 
+            set equal to the experience part of the profile STATE*/}
+          <Experience experience={profile.experience} />
+
+          <Education education={profile.education} />
+
+          {/* <Company company={profile.com} /> */}
+
+          <div className="my-2">
+            <button className="btn btn-danger" onClick={() => deleteAccount()}>
+              <i className="fas fa-user-minus" /> Delete My Account
+            </button>
+          </div>
+
+          {/* <ComAccounts /> */}
+          <DashboardAdminActions />
+        </Fragment>
+      ) : (
+        <Fragment>
+          <p>You have not yet setup a profile, please add some info</p>
+          <Link to="/create-profile" className="btn btn-primary my-1">
+            Create Individual Profile
+          </Link>
+          {/* Update Route */}
+          <Link to="/add-comprofile" className="btn btn-secondary my-1">
+            Create Company Profile
+          </Link>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  deleteAccount: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  profile: state.profile
+});
+
+export default connect(
+  mapStateToProps,
+  { getCurrentProfile, deleteAccount }
+)(Dashboard);
---
+++ models/Profile.js
@@ -0,0 +1,190 @@
+const mongoose = require("mongoose");
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user"
+  },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  address: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  githubusername: {
+    type: String
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      company: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      },
+      comsocial: {
+        youtube: {
+          type: String
+        },
+        twitter: {
+          type: String
+        },
+        facebook: {
+          type: String
+        },
+        linkedin: {
+          type: String
+        },
+        instagram: {
+          type: String
+        }
+      }
+    }
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  comprofile: [
+    {
+      companyname: {
+        type: String
+      },
+      website: {
+        type: String
+      },
+      location: {
+        type: String
+      },
+      address: {
+        type: String
+      },
+      tagline: {
+        type: String
+      },
+      industries: {
+        type: [String]
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  jobpost: [
+    {
+      jobTitle: {
+        type: String
+      },
+      companyName: {
+        type: String
+      },
+      workLocation: {
+        type: String
+      },
+      jobDescription: {
+        type: String
+      },
+      jobType: {
+        type: String
+      },
+      desiredExperience: {
+        type: String
+      },
+      salary: {
+        type: String
+      },
+      keySkills: {
+        type: [String]
+      },
+      posted: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
